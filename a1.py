a631b7c3ea41efc327dda6e69e9cebfe7e85afbe
Shaurya21032014 <haritika.arora@gmail.com>
2025-11-15 15:21:13 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,21 @@
+def codingal(name , age):
+    print(f"my name is {name} and i am {age} years old")
+
+codingal("codingal" , 6)
+
+
+
+
+def addition(num1, num2):
+    res = num1 + num2
+    return res
+
+
+print("the addition res is ", addition(10,20))
+    
+
+
+
+
+
+
